6a9f5e37421b6530e10d2f44f8642d044fc0332c
scaziti <rscaziti@gmail.com>
2021-01-27 13:32:10 -0300

    Criada pasta e adicionado exercícios da lista 1 - Java (Generation

---
+++ java/lista1/src/lista1/EvenOrOddPowSqrt.java
@@ -0,0 +1,27 @@
+package lista1;
+
+import java.util.Scanner;
+import java.text.*;
+
+public class EvenOrOddPowSqrt 
+{
+	public static void main(String[] args)
+	{
+		Scanner leitor = new Scanner(System.in);
+		DecimalFormat arredondar = new DecimalFormat("0.00");
+		
+		double numDigitado;
+		
+		System.out.println("Informe o número que deseja: ");
+		numDigitado = leitor.nextDouble();
+		
+		if (numDigitado % 2 == 0)
+		{
+			System.out.println("Número Par. A raíz quadrada será de " + arredondar.format(Math.sqrt(numDigitado)));
+		}
+		else if (numDigitado % 2 == 1)
+		{
+			System.out.println("Número ímpar. Esse número elevado a 2 será de " + arredondar.format(Math.pow(numDigitado, 2)));
+		}
+	}
+}
